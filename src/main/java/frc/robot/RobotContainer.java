5b44eeff960ae6d12f7848027a8070482dde5a8b
Hudsonhurtig@yahoo.com <johndough1432@gmail.com>
2022-02-07 16:07:42 -0600

    spacing

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -75,14 +75,23 @@ public class RobotContainer {
   }
 
   private static double deadband(double value, double deadband) {
+
     if (Math.abs(value) > deadband) {
+
       if (value > 0.0) {
+
         return (value - deadband) / (1.0 - deadband);
+
       } else {
+
         return (value + deadband) / (1.0 - deadband);
+
       }
+
     } else {
+
       return 0.0;
+      
     }
   }
 
