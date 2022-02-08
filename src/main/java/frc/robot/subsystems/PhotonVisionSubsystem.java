3256136f13c752692da3f8dafb9b84f5556d9e88
Cooper <cooperwright621@gmail.com>
2022-02-07 21:13:17 -0600

    Update PhotonVisionSubsystem.java

--- src/main/java/frc/robot/subsystems/PhotonVisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/PhotonVisionSubsystem.java
@@ -1,23 +1,11 @@
 package frc.robot.subsystems;
 
-
-import edu.wpi.first.math.controller.PIDController;
-
-import edu.wpi.first.math.util.Units;
-
-import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj.XboxController;
-
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-
-import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
-
 import org.photonvision.PhotonCamera;
 
 
-public class PhotonVision extends SubsystemBase {
+public class PhotonVisionSubsystem extends SubsystemBase {
 
 
     // Change this to match the name of your camera
