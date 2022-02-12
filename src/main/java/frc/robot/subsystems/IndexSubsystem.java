99b5062a21e2a4e267ec2ef06e4ec85957d0eb21
Mr-A768 <92965857+Mr-A768@users.noreply.github.com>
2022-02-12 08:37:14 -0600

    1.0.1

---
+++ src/main/java/frc/robot/subsystems/IndexSubsystem.java
@@ -0,0 +1,130 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+ 
+package frc.robot.subsystems;
+ 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+ 
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax.ControlType;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+ 
+public class IndexSubsystem extends SubsystemBase {
+  /** Creates a new ExampleSubsystem. */
+  private static final int deviceID = 1;
+  private CANSparkMax m_motor;
+  private SparkMaxPIDController m_pidController;
+  private RelativeEncoder m_encoder;
+  public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput, maxRPM;
+ 
+  public IndexSubsystem() {
+    // initialize motor
+    m_motor = new CANSparkMax(2, MotorType.kBrushless);
+ 
+    /**
+     * The restoreFactoryDefaults method can be used to reset the configuration
+     * parameters
+     * in the SPARK MAX to their factory default state. If no argument is passed,
+     * these
+     * parameters will not persist between power cycles
+     */
+    m_motor.restoreFactoryDefaults();
+ 
+    /**
+     * In order to use PID functionality for a controller, a SparkMaxPIDController
+     * object
+     * is constructed by calling the getPIDController() method on an existing
+     * CANSparkMax object
+     */
+    m_pidController = m_motor.getPIDController();
+ 
+    // Encoder object created to display position values
+    m_encoder = m_motor.getEncoder();
+ 
+    // PID coefficients
+    kP = 0.0001;
+    kI = 0.000001;
+    kD = 0;
+    kIz = 0;
+    kFF = 0;
+    kMaxOutput = 0.2;
+    kMinOutput = -1;
+    maxRPM = 600;
+ 
+    // set PID coefficients
+    m_pidController.setP(kP);
+    m_pidController.setI(kI);
+    m_pidController.setD(kD);
+    m_pidController.setIZone(kIz);
+    m_pidController.setFF(kFF);
+    m_pidController.setOutputRange(kMinOutput, kMaxOutput);
+ 
+    // display PID coefficients on SmartDashboard
+    SmartDashboard.putNumber("P Gain", kP);
+    SmartDashboard.putNumber("I Gain", kI);
+    SmartDashboard.putNumber("D Gain", kD);
+    SmartDashboard.putNumber("I Zone", kIz);
+    SmartDashboard.putNumber("Feed Forward", kFF);
+    SmartDashboard.putNumber("Max Output", kMaxOutput);
+    SmartDashboard.putNumber("Min Output", kMinOutput);
+    SmartDashboard.putNumber("Set Rotations", maxRPM);
+  }
+ 
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    // read PID coefficients from SmartDashboard
+    double p = SmartDashboard.getNumber("P Gain", 0);
+    double i = SmartDashboard.getNumber("I Gain", 0);
+    double d = SmartDashboard.getNumber("D Gain", 0);
+    double iz = SmartDashboard.getNumber("I Zone", 0);
+    double ff = SmartDashboard.getNumber("Feed Forward", 0);
+    double max = SmartDashboard.getNumber("Max Output", 0);
+    double min = SmartDashboard.getNumber("Min Output", 0);
+    double maxRPM = SmartDashboard.getNumber("Set Rotations", 0);
+ 
+    // if PID coefficients on SmartDashboard have changed, write new values to
+    // controller
+    if ((p != kP)) {
+      m_pidController.setP(p);
+      kP = p;
+    }
+    if ((i != kI)) {
+      m_pidController.setI(i);
+      kI = i;
+    }
+    if ((d != kD)) {
+      m_pidController.setD(d);
+      kD = d;
+    }
+    if ((iz != kIz)) {
+      m_pidController.setIZone(iz);
+      kIz = iz;
+    }
+    if ((ff != kFF)) {
+      m_pidController.setFF(ff);
+      kFF = ff;
+    }
+    if ((max != kMaxOutput) || (min != kMinOutput)) {
+      m_pidController.setOutputRange(min, max);
+      kMinOutput = min;
+      kMaxOutput = max;
+    }
+ 
+ 
+    m_pidController.setReference(maxRPM, ControlType.kVelocity);
+    double velocity = m_encoder.getVelocity();
+    SmartDashboard.putNumber("RPM Variable", velocity);
+  }
+ 
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
+ 
+
