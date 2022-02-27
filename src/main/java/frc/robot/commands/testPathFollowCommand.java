bba36407254c5546a01f4de2c3c914ee98c272c4
Hudsonhurtig@yahoo.com <johndough1432@gmail.com>
2022-02-27 17:31:45 -0600

    added path following

---
+++ src/main/java/frc/robot/commands/testPathFollowCommand.java
@@ -0,0 +1,72 @@
+package frc.robot.commands;
+
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.PathPlannerTrajectory.PathPlannerState;
+import com.pathplanner.lib.PathPlannerTrajectory;
+
+import edu.wpi.first.math.controller.HolonomicDriveController;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DrivetrainSubsystem;
+
+/** Trajectory following */
+public class testPathFollowCommand extends CommandBase {
+
+  @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
+  private final DrivetrainSubsystem m_DrivetrainSubsystem;
+  private PathPlannerTrajectory trajectory;
+  private HolonomicDriveController controller;
+  private final Timer timer = new Timer();
+
+  public testPathFollowCommand(DrivetrainSubsystem m_DrivetrainSubsystem) {
+    this.m_DrivetrainSubsystem = m_DrivetrainSubsystem;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(m_DrivetrainSubsystem);
+  }
+
+  @Override
+  public void initialize() {
+    trajectory = PathPlanner.loadPath("Test", 0.8, 1);
+
+    ProfiledPIDController thetaController = new ProfiledPIDController(.5, 0, 0,
+        new TrapezoidProfile.Constraints(Math.PI, Math.PI));
+    PIDController xPosPidController = new PIDController(1, 0, 0);
+    PIDController yPosPidController = new PIDController(1, 0, 0);
+
+    thetaController.enableContinuousInput(-Math.PI, Math.PI);
+
+    controller = new HolonomicDriveController(xPosPidController, yPosPidController, thetaController);
+
+    m_DrivetrainSubsystem.setOdomToAutoPose(new Translation2d(7.5, 1.75), new Rotation2d(-90));
+
+    timer.reset();
+    timer.start();
+  }
+
+  @Override
+  public void execute() {
+    PathPlannerState desiredState = (PathPlannerState) trajectory.sample(timer.get());
+
+    ChassisSpeeds targetChassisSpeeds = controller.calculate(m_DrivetrainSubsystem.getPose(), desiredState,
+        desiredState.poseMeters.getRotation());
+
+        m_DrivetrainSubsystem.drive(targetChassisSpeeds);
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    m_DrivetrainSubsystem.drive(new ChassisSpeeds(0, 0, 0));
+    timer.stop();
+  }
+
+  @Override
+  public boolean isFinished() {
+    return timer.hasElapsed(trajectory.getTotalTimeSeconds());
+  }
+}
